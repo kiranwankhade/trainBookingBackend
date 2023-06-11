9ab8ca7c45d8cf2c316d9c08acc8234417f1f22b
kiranwankhade <kiranwankhade7738@gamil.com>
2023-06-11 16:34:04 +0530

    backend

---
+++ Model/Seat.Model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+// Seat Reservation Schema
+const reservationSchema = new mongoose.Schema({
+    coachRow: Number,
+    seatNumber: String,
+    isReserved: Boolean,
+},{
+    versionKey:false
+});
+
+
+
+const ReserveModel = mongoose.model("seatsbooks",reservationSchema);
+
+module.exports = {
+    ReserveModel
+}
+
+
